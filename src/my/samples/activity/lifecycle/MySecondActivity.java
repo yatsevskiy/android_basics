c867e79261d3a1d02acecf5d26541533a656d0de
v.yatsevskiy@gmail.com <v.yatsevskiy@gmail.com>
2013-05-06 13:03:13 +0300

    hide/show animation has been add for fragment sample

--- src/my/samples/activity/lifecycle/MySecondActivity.java
+++ src/my/samples/activity/lifecycle/MySecondActivity.java
@@ -3,6 +3,8 @@ package my.samples.activity.lifecycle;
 import my.samples.fragment.lifecycle.MyFragment;
 import my.samples.service.lifecycle.R;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +30,29 @@ public class MySecondActivity extends MyBaseActivity {
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 		ft.add(R.id.my_fragment_layout_1, new MyFragment());
 		ft.commit();
+
+		findViewById(R.id.hide_fragment_1).setOnClickListener(
+				new OnClickListener() {
+					public void onClick(View v) {
+						FragmentManager fragmentManager = getSupportFragmentManager();
+
+						Fragment fragment = fragmentManager
+								.findFragmentById(R.id.my_fragment_layout_1);
+
+						FragmentTransaction ft = fragmentManager
+								.beginTransaction();
+
+						ft.setCustomAnimations(android.R.anim.slide_in_left,
+								android.R.anim.slide_in_left);
+
+						if (fragment.isHidden()) {
+							ft.show(fragment);
+						} else {
+							ft.hide(fragment);
+						}
+						ft.commit();
+					}
+				});
 
 		findViewById(R.id.push_fragment).setOnClickListener(
 				new OnClickListener() {
